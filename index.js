1fe41c6a3772b36906b5781517856b47d17e045a
Vanessa Porras <vanessapcg@gmail.com>
2020-10-21 03:38:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,16 +22,14 @@ function sayHiToGrandma (string){
   
   if (string===string.toLowerCase())
   {
-  return "I can't hear you!"
+      return "I can't hear you!"
   }
-  else if (shout(string)===string.toUpperCase())
+  else if (string===string.toUpperCase())
   {
-  
-  return "YES INDEED!"
+      return "YES INDEED!"
   }
   else if (string==='I love you, Grandma.')
   {
-  
-  return "I love you, too."
+      return "I love you, too."
   }
 }
